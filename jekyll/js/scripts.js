2d6005cbd506256cc8ad5c1a4d9557ef4a5bd32d
Aaron Kapor <aaronkapor@gmail.com>
2025-05-18 05:07:54 -0700

    overdue commit

---
+++ jekyll/js/scripts.js
@@ -0,0 +1,58 @@
+
+
+
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    document.getElementById("logo_tab").style.margin = "0px";
+    document.getElementById("logo_mob").style.margin = "0px";
+  } else {
+    document.getElementById("logo_tab").style.margin = "20px";
+    document.getElementById("logo_mob").style.margin = "8px";
+  }
+}
+
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
+
+/* Optional: Add active class to the current button (highlight it) */
+var container = document.getElementById("btnContainer");
+var btns = container.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+
+
+
+
+
+
+
